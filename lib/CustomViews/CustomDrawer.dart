4b254144a865cc116bb46545ff4987afdbecf7df
CharLyModZz288 <126071634+CharLyModZz288@users.noreply.github.com>
2024-03-02 19:21:45 +0100

    estableciendo diseños

---
+++ lib/CustomViews/CustomDrawer.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+
+
+  Function(int indice)? onItemTap;
+  String imagen;
+
+  CustomDrawer({Key? key, required this.onItemTap, required this.imagen
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.black,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.network(
+                  imagen,
+                  width: 100,
+                  height: 100,
+                ),
+                Text(
+                  "Selecciona lo que desees",
+                  style: TextStyle(color: Colors.red,
+                      fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            selectedColor: Colors.yellow,
+            selected: true,
+            title: const Text('Perfil'),
+            onTap: () {
+              onItemTap!(1);
+            },
+          ),
+          ListTile(
+            title: const Text('Cerrar Sesion'),
+            onTap: () {
+              onItemTap!(0);
+            },
+          ),
+
+          ListTile(
+            title: const Text('Consultar Mapa'),
+            onTap: () {
+              onItemTap!(3);
+            },
+          ),
+
+          ListTile(
+            title: const Text('Busqueda Titulo'),
+            onTap: () {
+              onItemTap!(6);
+            },
+          ),
+          ListTile(
+            title: const Text('Ajustes'),
+            onTap: () {
+              onItemTap!(7);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/CustomViews/CustomGredCellView.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../Home/EditarPost.dart';
+
+
+class CustomGredCellView extends StatelessWidget{
+
+  final String sText;
+  final String usuario;
+  final String tituloPost;
+  final int iColorCode;
+  final String imagen;
+  final double dFontSize;
+  final String idPost;
+
+  const CustomGredCellView({super.key,
+    required this.sText,
+    required this.iColorCode,
+    required this.imagen,
+    required this.dFontSize,
+    required this.usuario,
+    required this.idPost,
+    required this.tituloPost});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 1,
+        mainAxisSpacing: 200.0,
+        crossAxisSpacing: 200.0,
+      ),
+      padding: const EdgeInsets.all(8.0),
+      itemCount: 1,
+      itemBuilder: (BuildContext context, int index) {
+        return InkWell(
+          onTap: () {
+            print("Clic en la imagen");
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => EditarPost(postId: idPost,usuario: usuario, imagen: imagen),
+              ),
+            );
+          },
+          child: Container(
+            color: Colors.orangeAccent, // Cambié el color a beige
+            child: Stack(
+              children: [
+                if (imagen.isNotEmpty) ...[
+                  SizedBox(
+                    width: double.infinity,
+                    child: Image.network(
+                      imagen,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      color: Colors.black.withOpacity(0.5),
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        usuario,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12.0,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ],
+                Positioned(
+                  bottom: 16.0,
+                  left: 5,
+                  child: Text(
+                    tituloPost,
+                    style: TextStyle(
+                      fontSize: dFontSize,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
