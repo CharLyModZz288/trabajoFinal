8d77d1f006053ce25c08f842d807c31fcc38e443
carlos.escriva <escrivasegoviac@gmail.com>
2024-04-05 13:07:00 +0200

    Creación de un nuevo CustomDrawer.dart

---
+++ lib/CustomViews/CustomDrawer2.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer2 extends StatelessWidget {
+
+
+  Function(int indice)? onItemTap;
+  String imagen;
+
+  CustomDrawer2({Key? key, required this.onItemTap, required this.imagen
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.black,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.network(
+                  imagen,
+                  width: 100,
+                  height: 100,
+                ),
+                Text(
+                  "Selecciona lo que desees",
+                  style: TextStyle(color: Colors.red,
+                      fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            selectedColor: Colors.yellow,
+            selected: true,
+            title: const Text('Perfil'),
+            onTap: () {
+              onItemTap!(0);
+            },
+          ),
+          ListTile(
+            title: const Text('Cerrar Sesion'),
+            onTap: () {
+              onItemTap!(1);
+            },
+          ),
+
+          ListTile(
+            title: const Text('Consultar Mapa'),
+            onTap: () {
+              onItemTap!(2);
+            },
+          ),
+
+          ListTile(
+            title: const Text('Busqueda Titulo'),
+            onTap: () {
+              onItemTap!(3);
+            },
+          ),
+          ListTile(
+            title: const Text('Ajustes'),
+            onTap: () {
+              onItemTap!(4);
+            },
+          ),
+          ListTile(
+            title: const Text('Inicio'),
+            onTap: () {
+              onItemTap!(5);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
