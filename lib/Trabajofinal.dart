a06c2eb9361a905932dfe410fe94fe60f031f6dd
CharLyModZz288 <CharLyModZz288>
2024-04-10 10:37:40 +0200

    Cambios modificados

--- lib/Trabajofinal.dart
+++ lib/Trabajofinal.dart
@@ -9,9 +9,13 @@ import 'package:trabajofinal/splash/SplashView.dart';
 import 'Home/EditarPost.dart';
 import 'Home/Editarperfil.dart';
 import 'Home/HomeView.dart';
+import 'Home/HomeView3.dart';
+import 'Home/HomeView4.dart';
 import 'Home/MapaView.dart';
 import 'Home/PostCreateView.dart';
 import 'Home/PostCreateView2.dart';
+import 'Home/PostCreateView3.dart';
+import 'Home/PostCreateView4.dart';
 import 'Singletone/DataHolder.dart';
 import 'onBoarding/LoginView.dart';
 import 'onBoarding/PerfilView.dart';
@@ -32,8 +36,12 @@ class MuseoYismer extends StatelessWidget{
         '/perfilview':(context) => PerfilView(),
         '/homeview':(context) => HomeView(),
         '/homeview2':(context) => HomeView2(),
+        '/homeview3':(context) => HomeView3(),
+        '/homeview4':(context) => HomeView4(),
         '/postcreateview':(context) => PostCreateView(),
         '/postcreateview2':(context) => PostCreateView2(),
+        '/postcreateview3':(context) => PostCreateView3(),
+        '/postcreateview4':(context) => PostCreateView4(),
         '/editarpost':(context) => EditarPost(),
         '/mapaview':(context) => MapaView(),
         '/editarperfil':(context) => Editarperfil(),
