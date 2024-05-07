89af6906458cda2e5de445cf3a15f5aa9e024110
carlos.escriva <escrivasegoviac@gmail.com>
2024-05-07 14:28:14 +0200

    Cambios en editar la publicacion para el usuario y el administrador

--- lib/Home/EditarPost.dart
+++ lib/Home/EditarPost.dart
@@ -25,16 +25,16 @@ class EditarPost extends StatefulWidget {
 }
 
 class _EditarPostState extends State<EditarPost> {
-  TextEditingController _tituloController = TextEditingController(); // Agregado
-  TextEditingController _contenidoController = TextEditingController(); // Agregado
+  TextEditingController _tituloController = TextEditingController();
+  TextEditingController _contenidoController = TextEditingController();
   ImagePicker _picker = ImagePicker();
   FirebaseFirestore db = FirebaseFirestore.instance;
   File _imagePreview = File("");
   late FbPostId post;
   String userId = " ";
   String usuario = " ";
-  String titulo = " ";
-  String postContenido = " ";
+  String titulo = " "; // Inicializa la variable
+  String postContenido = " "; // Inicializa la variable
   String imagenUrl = " ";
   DataHolder conexion = DataHolder();
   bool _isDisposed = false;
@@ -42,82 +42,24 @@ class _EditarPostState extends State<EditarPost> {
   @override
   void initState() {
     super.initState();
-    _tituloController.text = widget.tituloInicial ?? ''; // Agregado
-    _contenidoController.text = widget.contenidoInicial ?? ''; // Agregado
-    imagenUrl = widget.imagen ?? ""; // Agregado
+    // Inicializa los controladores de texto y las variables
+    _tituloController.text = widget.tituloInicial ?? '';
+    _contenidoController.text = widget.contenidoInicial ?? '';
+
+    // Establece los valores iniciales para titulo y postContenido
+    titulo = widget.tituloInicial ?? '';
+    postContenido = widget.contenidoInicial ?? '';
+
+    imagenUrl = widget.imagen ?? '';
   }
+
+
   @override
   void dispose() {
     _isDisposed = true;
     super.dispose();
-    print(widget.postId.toString()); // Use widget.postId here
-  }
-
-  Future<void> updateImage() async {
-    if (_isDisposed) return;
-
-    XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-    if (image != null) {
-      setState(() {
-        _imagePreview = File(image.path);
-      });
-
-      String newImageUrl = await setearUrlImagen();
-
-      if (!_isDisposed) {
-        setState(() {
-          imagenUrl = newImageUrl;
-        });
-      }
-    }
-  }
-
-
-  Future<void> updateImageCamera() async {
-
-    print("el usuario es:" + widget.postId.toString());
-    if (_isDisposed) return; // Verificar si el widget ha sido eliminado
-
-    XFile? image = await _picker.pickImage(source: ImageSource.camera);
-    if (image != null) {
-      setState(() {
-        _imagePreview = File(image.path);
-      });
-
-      String newImageUrl = await setearUrlImagen();
-
-      if (!_isDisposed) {
-        setState(() {
-          imagenUrl = newImageUrl;
-        });
-      }
-    }
-  }
-
-  Future<String> setearUrlImagen() async {
-    final storageRef = FirebaseStorage.instance.ref();
-
-    String rutaEnNube =
-        "posts/" + FirebaseAuth.instance.currentUser!.uid + "/imgs/" +
-            DateTime.now().millisecondsSinceEpoch.toString() + ".jpg";
-    print("RUTA DONDE VA A GUARDARSE LA IMAGEN: " + rutaEnNube);
-
-    final rutaAFicheroEnNube = storageRef.child(rutaEnNube);
-
-    final metadata = SettableMetadata(contentType: "image/jpeg");
-    try {
-      await rutaAFicheroEnNube.putFile(_imagePreview, metadata);
-
-      print("SE HA SUBIDO LA IMAGEN");
-
-      String url = await rutaAFicheroEnNube.getDownloadURL();
-      print("URL de la imagen: $url");
-      return url;
-    } on FirebaseException catch (e) {
-      print("ERROR AL SUBIR IMAGEN: " + e.toString());
-      print("STACK TRACE: " + e.stackTrace.toString());
-      return "";
-    }
+    // Imprimir el ID del post en el método dispose
+    print(widget.postId.toString());
   }
 
   @override
@@ -131,90 +73,23 @@ class _EditarPostState extends State<EditarPost> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Título: " + titulo),
-            Text("Contenido: " + postContenido),
+            // Muestra los valores de título y contenido
+            Text("Título: $titulo"),
+            Text("Contenido: $postContenido"),
             SizedBox(height: 20),
-            if (imagenUrl != " ")
+            // Muestra la imagen si la URL es válida
+            if (imagenUrl.isNotEmpty)
               Image.network(
                 imagenUrl,
                 width: double.infinity,
                 height: 200,
                 fit: BoxFit.contain,
               ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: ()  {
-                    // Mostrar un cuadro de diálogo para que el usuario ingrese nuevos datos
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text('Modificar Datos del Post'),
-                          content: Column(
-                            children: [
-                              TextField(
-                                onChanged: (value) {
-                                  setState(() {
-                                    titulo = value;
-                                  });
-                                },
-                                decoration: InputDecoration(labelText: 'Nuevo Título'),
-                              ),
-                              TextField(
-                                onChanged: (value) {
-                                  setState(() {
-                                    postContenido = value;
-                                  });
-                                },
-                                decoration: InputDecoration(labelText: 'Nuevo Contenido'),
-                              ),
-                              ElevatedButton(
-                                onPressed: () async {
-                                  await updateImageCamera();
-                                },
-                                child: Text('Seleccionar Imagen desde camera'),
-                              ),
-                              ElevatedButton(
-                                onPressed: () async {
-                                  await updateImage();
-                                },
-                                child: Text('Foto desde galeria'),
-                              ),
-                            ],
-                          ),
-                          actions: [
-                            ElevatedButton(
-                              onPressed: () {
-                                // Actualizar los datos en Firebase
-                                conexion.fbadmin.updatePostData(
-                                  titulo,
-                                  postContenido,
-                                  imagenUrl,
-                                  widget.usuario.toString(),
-                                  widget.postId.toString(),
-                                );
-                                // Cerrar el diálogo
-                                Navigator.of(context).pop();
-                                // Actualizar el estado para reflejar los cambios
-                                setState(() {});
-                              },
-                              child: Text('Guardar Cambios'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  child: Text('Modificar Datos del Post'),
-                ),
-
-              ],
-            ),
+            // Agrega más widgets o funciones según sea necesario
           ],
         ),
       ),
     );
   }
-  }
+
+}
