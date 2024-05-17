94266150eedb7e9806266745cdfd76121aaeafaa
carlos.escriva <escrivasegoviac@gmail.com>
2024-05-17 13:49:30 +0200

    Cambios generales

--- lib/splash/SplashView.dart
+++ lib/splash/SplashView.dart
@@ -27,41 +27,26 @@ class _SplashViewState extends State<SplashView>{
     super.initState();
     checkSession();
   }
-
   void checkSession() async{
     await Future.delayed(Duration(seconds: 0));
-
     if (FirebaseAuth.instance.currentUser != null) {
-
       String uid = FirebaseAuth.instance.currentUser!.uid;
-
       DocumentReference<FbUsuario> enlace = db.collection("Usuarios").doc(uid).withConverter(fromFirestore: FbUsuario.fromFirestore,
         toFirestore: (FbUsuario usuario, _) => usuario.toFirestore(),);
-
       FbUsuario usuario;
-
       DocumentSnapshot<FbUsuario> docSnap = await enlace.get();
       usuario = docSnap.data()!;
-
       if (usuario != null) {
-
         Navigator.of(context).popAndPushNamed("/homeview");
       }
-
       else{
-
         Navigator.of(context).popAndPushNamed("/perfilview");
       }
     }
-
     else{
       Navigator.of(context).popAndPushNamed("/loginview");
-
     }
-
   }
-
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
