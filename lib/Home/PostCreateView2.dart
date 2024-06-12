28d65e33ae67b2c4140d7f2d8c1b1e8fa6f8bc74
CharLyModZz288 <126071634+CharLyModZz288@users.noreply.github.com>
2024-06-12 22:16:36 +0200

    cambios generales

--- lib/Home/PostCreateView2.dart
+++ lib/Home/PostCreateView2.dart
@@ -81,7 +81,7 @@ class _PostCreateViewState extends State<PostCreateView2> {
       await rutaAFicheroEnNube.putFile(_imagePreview, metadata);
 
       print("SE HA SUBIDO LA IMAGEN");
-      Navigator.of(context).popAndPushNamed("/homeview2");
+      Navigator.of(context).popAndPushNamed("/youtubersadmin");
       // Obtén la URL de la imagen después de subirla
       String url = await rutaAFicheroEnNube.getDownloadURL();
       print("URL de la imagen: $url");
